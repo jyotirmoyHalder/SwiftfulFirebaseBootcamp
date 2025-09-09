74a7e7d1ef45b60ed93e369a94658d2396162e9f
Jyotirmoy Halder <jyotirmoy.cse5.bu@gmail.com>
2025-09-09 09:15:09 +0600

    SwiftfulFirebaseBootcamp

---
+++ SwiftfulFirebaseBootcamp/Authentication/AuthenticationView.swift
@@ -0,0 +1,18 @@
+//
+//  AuthenticationView.swift
+//  SwiftfulFirebaseBootcamp
+//
+//  Created by jyotirmoy_halder on 9/9/25.
+//
+
+import SwiftUI
+
+struct AuthenticationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AuthenticationView()
+}
