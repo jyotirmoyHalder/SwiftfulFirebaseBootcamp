ba52d4725359c28e5b9b1e2f55096e2c09cc1e71
Jyotirmoy Halder <jyotirmoy.cse5.bu@gmail.com>
2025-09-16 17:53:03 +0600

    [Feature] Added Filtering according to price and category and both combined

--- SwiftfulFirebaseBootcamp/Products/ProductsView.swift
+++ SwiftfulFirebaseBootcamp/Products/ProductsView.swift
@@ -11,11 +11,74 @@ import SwiftUI
 final class ProductsViewModel: ObservableObject {
     
     @Published private(set) var products: [Product] = []
+    @Published var selectedFilter: FilterOption? = nil
+    @Published var selectedCategory: CategoryOption? = nil
     
-    func getAllProducts() async throws {
-        self.products = try await ProductsManager.shared.getallProducts()
-    }
+//    func getAllProducts() async throws {
+//        self.products = try await ProductsManager.shared.getallProducts()
+//    }
 
+    enum FilterOption: String, CaseIterable {
+        case noFilter
+        case priceHigh
+        case priceLow
+        
+        var priceDescending: Bool? {
+            switch self {
+            case .noFilter: return nil
+            case .priceHigh: return true
+            case .priceLow: return false
+            }
+        }
+    }
+    
+    func filterSelected(option: FilterOption) async throws {
+        self.selectedFilter = option
+        self.getProducts()
+
+//        switch option {
+//        case .noFilter:
+//            self.products = try await ProductsManager.shared.getallProducts()
+//        case .priceHigh:
+//            self.products = try await ProductsManager.shared.getAllProductsSortedByPrice(descending: true)
+//        case .priceLow:
+//            self.products = try await ProductsManager.shared.getAllProductsSortedByPrice(descending: false)
+//        }
+        
+    }
+    
+    enum CategoryOption: String, CaseIterable {
+        case noCategory
+        case beauty
+        case fragrances
+        case furniture
+        case groceries
+        
+        var categoryKey: String? {
+            if self == .noCategory {
+                return nil
+            }
+            return self.rawValue
+        }
+    }
+    
+    func categorySelected(option: CategoryOption) async throws {
+        self.selectedCategory = option
+        self.getProducts()
+//        switch option {
+//        case .noCategory:
+//            self.products = try await ProductsManager.shared.getallProducts()
+//        case .beauty, .fragrances, .furniture, .groceries:
+//            self.products = try await ProductsManager.shared.getAllProductsForCategory(category: option.rawValue)
+//          
+//        }
+    }
+    
+    func getProducts() {
+        Task {
+            self.products = try await ProductsManager.shared.getAllProductsByPrice(priceDescending: selectedFilter?.priceDescending, forCategory: selectedCategory?.categoryKey)
+        }
+    }
 }
 
 struct ProductsView: View {
@@ -29,8 +92,33 @@ struct ProductsView: View {
             }
         }
         .navigationTitle("Products")
-        .task {
-            try? await viewModel.getAllProducts()
+        .toolbar(content: {
+            ToolbarItem(placement: .topBarLeading) {
+                Menu("Filter: \(viewModel.selectedFilter?.rawValue ?? "NONE")") {
+                    ForEach(ProductsViewModel.FilterOption.allCases, id: \.self) { option in
+                        Button(option.rawValue) {
+                            Task {
+                                try? await viewModel.filterSelected(option: option)
+                            }
+                        }
+                    }
+                }
+            }
+            
+            ToolbarItem(placement: .topBarTrailing) {
+                Menu("Filter: \(viewModel.selectedCategory?.rawValue ?? "NONE")") {
+                    ForEach(ProductsViewModel.CategoryOption.allCases, id: \.self) { option in
+                        Button(option.rawValue) {
+                            Task {
+                                try? await viewModel.categorySelected(option: option)
+                            }
+                        }
+                    }
+                }
+            }
+        })
+        .onAppear {
+            viewModel.getProducts()
         }
     }
 }
